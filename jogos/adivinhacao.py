236656b65469ace4a7641d347c1e24b5a2c3d655
José Bezerra <josbezfi@gmail.com>
2023-09-11 15:12:27 -0300

    Criando jogo de adivinhação com o while

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -1,1 +1,29 @@
+print('********************')
 print("Jogo de adivinhação")
+print('********************')
+numero_secreto = 45
+
+total_tentativas = 3
+rodada = 1
+while rodada <= total_tentativas:
+    print("Tentativa {} de {}".format(rodada, total_tentativas))
+
+    chute = input("digite o seu número: ")
+    print("Você digitou: ", chute)
+    chute = int(chute)
+
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
+
+    if numero_secreto == chute:
+        print('Você acertou!')
+    else:
+        if maior:
+            print('Você errou! Seu chute foi maior que o número secreto!')
+        elif menor:
+            print('Você errou! Seu chute foi menor que o número secreto!')
+
+    rodada = rodada + 1
+
+print("Fim de Jogo!")
