9e0422ace5e1d66ebb63ad346e364f71eb7a61c2
José Bezerra <josbezfi@gmail.com>
2023-09-11 15:44:02 -0300

    Utilizando o for para o laço

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -4,13 +4,17 @@ print('********************')
 numero_secreto = 45
 
 total_tentativas = 3
-rodada = 1
-while rodada <= total_tentativas:
+
+for rodada in range(1, total_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_tentativas))
 
     chute = input("digite o seu número: ")
     print("Você digitou: ", chute)
     chute = int(chute)
+
+    if chute < 1 or chute > 100:
+        print("Você deve digitar um número entre 0 e 100!")
+        continue
 
     acertou = chute == numero_secreto
     maior = chute > numero_secreto
@@ -18,12 +22,12 @@ while rodada <= total_tentativas:
 
     if numero_secreto == chute:
         print('Você acertou!')
+        break
     else:
         if maior:
             print('Você errou! Seu chute foi maior que o número secreto!')
         elif menor:
             print('Você errou! Seu chute foi menor que o número secreto!')
 
-    rodada = rodada + 1
 
 print("Fim de Jogo!")
