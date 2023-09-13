71c951a2eccf5f7c9eaa1969d4e88231f9e2e373
José Bezerra <josbezfi@gmail.com>
2023-09-12 22:07:22 -0300

    craindo o jogo da forca

--- jogos/forca.py
+++ jogos/forca.py
@@ -3,6 +3,23 @@ def jogar():
     print("***** Jogo da Forca *******")
     print("***************************")
 
+    palavra_secreta = "banana"
+    letras_certas = ["_", "_", "_", "_", "_", "_"]
+    enforcou = False
+    acertou = False
+
+    while not enforcou and not acertou:
+        chute = input("Qual a letra?")
+        chute = chute.strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if chute.upper() == letra.upper():
+                print("Encontrei a letra '{}' na posição {}".format(letra, index))
+            index = index + 1
+
+        print("jogando...")
+
 
 if __name__ == "__main__":
     jogar()
