b8fa97f0c0ccb2aa524bac8cf54643dfd9cc5ad7
José Bezerra <josbezfi@gmail.com>
2023-09-14 20:24:29 -0300

    implementando ifs para encerrar o jogo

--- jogos/forca.py
+++ jogos/forca.py
@@ -3,25 +3,38 @@ def jogar():
     print("***** Jogo da Forca *******")
     print("***************************")
 
-    palavra_secreta = "banana"
+    palavra_secreta = "banana".upper()
     letras_certas = ["_", "_", "_", "_", "_", "_"]
     enforcou = False
     acertou = False
+    erros = 0
 
     print(letras_certas)
     while not enforcou and not acertou:
-        chute = input("Qual a letra?")
-        chute = chute.strip()
+        chute = input("Qual a letra? ")
+        chute = chute.strip().upper()
 
-        index = 0
-        for letra in palavra_secreta:
-            if chute.upper() == letra.upper():
-                letras_certas[index] = letra
-                print("Encontrei a letra '{}' na posição {}".format(letra, index))
-            index = index + 1
+        if chute in palavra_secreta:
+            index = 0
+            for letra in palavra_secreta:
+                if chute == letra:
+                    letras_certas[index] = letra
+                    print("Encontrei a letra '{}' na posição {}".format(letra, index))
+                index = index + 1
+        else:
+            erros = erros + 1
+            print("Ops, você errou! Faltam {} tentativas.".format(6 - erros))
+
+        enforcou = erros == 6
+        acertou = "_" not in letras_certas
         print(letras_certas)
 
         print("jogando...")
+
+    if acertou:
+        print("Você ganhou!")
+    else:
+        print("Você perdeu!")
 
 
 if __name__ == "__main__":
