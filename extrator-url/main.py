2cd25a71574226a8fdd534e0226dd415ba2ac406
José Bezerra <50915483+Jose-Bezerra@users.noreply.github.com>
2023-10-08 16:33:52 -0300

    Refatorando o código com princípios de OO.

--- extrator-url/main.py
+++ extrator-url/main.py
@@ -1,5 +1,12 @@
-url = 'https://bytebank.com/cambio?moedaDestino=dolar&moedaOrigem=real'
-print(url)
+# url = 'https://bytebank.com/cambio?moedaDestino=dolar&moedaOrigem=real'
+url = ""
+
+# Sanitizando uma url (Removendo espaços em brancos e caracteres especiais)
+url = url.strip()
+
+# Validando a url
+if url == "":
+    raise ValueError("A URL está vazia")
 
 # Separa base dos parâmetros
 indice_interrogacao = url.find('?')
