c23b90f16166dbb1892fae0935d61fb58a89ef35
José Bezerra <50915483+Jose-Bezerra@users.noreply.github.com>
2023-10-08 11:28:42 -0300

    Implementando a função find().

--- extrator-url/main.py
+++ extrator-url/main.py
@@ -1,6 +1,12 @@
-url = 'bytebank.com/cambio?moedaOrigem=real'
-url_base = url[0:19]
-url_parametros = url[20:36]
+url = 'https://bytebank.com/cambio?moedaOrigem=real'
 print(url)
+indice_interrogacao = url.find('?')
+
+# O primeiro parâmetro é omitido também.
+url_base = url[:indice_interrogacao]
 print(url_base)
+
+# O primeiro parâmetro do fatiamento da url é acrescentado de 1 porque ele
+# é inclusivo e ira imprimir o '?. O segundo parâmetro será omitido'
+url_parametros = url[indice_interrogacao + 1:]
 print(url_parametros)
