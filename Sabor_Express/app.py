3ded6fa4c4813190ef1058f758d37dc3c0932fe8
José Bezerra <50915483+Jose-Bezerra@users.noreply.github.com>
2023-12-25 16:01:59 -0300

    Update app.py
    Módulos e funções

--- Sabor_Express/app.py
+++ Sabor_Express/app.py
@@ -1,6 +1,8 @@
+import os
 
 
-print(""""
+def exibir_nome_programa():
+    print(""""
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
 ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
 ╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
@@ -9,10 +11,37 @@ print(""""
 ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░
       """)
 
-print('1. Cadastrar Restaurante')
-print('2. Listar Restaurante')  
-print('3. Ativar Restaurante')  
-print('4. Sair\n')  
 
-opcao_escolhida = input('Escolha uma opção: ')
-print(f'Você escolheu a opção: {opcao_escolhida}')
+def exibir_opcoes():
+    print('1. Cadastrar Restaurante')
+    print('2. Listar Restaurante')
+    print('3. Ativar Restaurante')
+    print('4. Sair\n')
+
+
+def finalizar_app():
+    os.system('clear')
+    print('Finalizando o App!')
+
+
+def escolher_opcao():
+    opcao_escolhida = int(input('Escolha uma opção: '))
+
+    if opcao_escolhida == 1:
+        print('Cadastrar Restaurante')
+    elif opcao_escolhida == 2:
+        print('Listar Restaurantes')
+    elif opcao_escolhida == 3:
+        print('Ativar Restaurante')
+    else:
+        finalizar_app()
+
+
+def main():
+    exibir_nome_programa()
+    exibir_opcoes()
+    escolher_opcao()
+
+
+if __name__ == '__main__':
+    main()
